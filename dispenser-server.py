cbfa47068f1d05498cc0691fb16e77d1399f8c0f
Nick <nicholasg1989@gmail.com>
2020-08-30 02:50:01 +0100

    add webpage

--- dispenser-server.py
+++ dispenser-server.py
@@ -1,22 +1,31 @@
 import RPi.GPIO as GPIO
 import time
+from flask import Flask, render_template
+import threading
+
+
+app = Flask('dispener-server')
+lock = threading.Lock()
+last_treat = 0
+TREAT_MIN_SECONDS = 2
+
 GPIO.setmode(GPIO.BOARD)
 control_pins = [7,11,13,15]
 
 for pin in control_pins:
-  GPIO.setup(pin, GPIO.OUT)
-  GPIO.output(pin, 0)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, 0)
 
 halfstep_seq = [
-  [0,1,0,1],
-  [0,0,0,1],
-  [1,0,0,1],
-  [1,0,0,0],
-  [1,0,1,0],
-  [0,0,1,0],
-  [0,1,1,0],
-  [0,1,0,0],
-]
+        [0,1,0,1],
+        [0,0,0,1],
+        [1,0,0,1],
+        [1,0,0,0],
+        [1,0,1,0],
+        [0,0,1,0],
+        [0,1,1,0],
+        [0,1,0,0],
+        ]
 
 def get_step(n):
     x = n % len(halfstep_seq)
@@ -29,18 +38,31 @@ def stop():
       GPIO.output(control_pins[pin], 1)
 
 def step():
-  global pos
-  for i in range(pos, pos+33):
-      s = get_step(i)
-      for pin in range(len(control_pins)):
-        GPIO.output(control_pins[pin], 1-s[pin])
-      time.sleep(.005)
-  time.sleep(.1)
-  stop()
-  pos = pos + 33
-  if pos % 100 == 99:
-      pos += 1
+    with lock:
+        global pos
+        global last_treat
 
-while True:
-  time.sleep(10)
-  step()
+        if time.time() - last_treat < TREAT_MIN_SECONDS:
+            return
+
+        last_treat = time.time()
+        for i in range(pos, pos+33):
+            s = get_step(i)
+            for pin in range(len(control_pins)):
+              GPIO.output(control_pins[pin], 1-s[pin])
+            time.sleep(.005)
+        time.sleep(.1)
+        stop()
+        pos = pos + 33
+        if pos % 100 == 99:
+            pos += 1
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/step', methods=['POST'])
+def call_step():
+    step()
+    return 'ok'
